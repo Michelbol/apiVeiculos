6d83cbb59a44c50153793c21e3ba4f3f4e7f0f67
Michel <michel.bolzon123@gmail.com>
2018-01-18 17:30:18 -0200

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function registrar(Request $request){
+        $dados = $request->all();
+        if(!User::where('email', $dados['email'])->count()){
+            $dados['password'] = bcrypt($dados['password']);
+            $user = User::create($dados);
+            return response()->json(['data'=>$user], 201);
+        }else{
+            return response()->json(['message'=>'Este e-mail já está cadastrado.'], 400);
+        }
+    }
+}
